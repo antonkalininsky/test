101ba3d0fe430db67fdcad323e69cd046d62fc16
Anton <chadik.free@gmail.com>
2023-06-24 11:52:18 +0600

    Рекурсивная отрисовка списка

---
+++ src/components/TableRow.vue
@@ -0,0 +1,22 @@
+<script setup>
+// import TableRow from './TableRow.vue'
+
+// PROPS
+const props = defineProps(['data'])
+</script>
+<template>
+  <template v-for="item in props.data">
+    <tr class="row">
+      <td><button>></button>{{ item.title }}</td>
+      <td>calc value</td>
+      <td>{{ item.value }}</td>
+      <td>CRUD</td>
+    </tr>
+    <TableRow :data="item.children"/>
+  </template>
+</template>
+<style scoped>
+.row td {
+  padding: 10px 20px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,89 @@
-<script setup></script>
+<script setup>
+import TableRow from '../components/TableRow.vue'
+import { ref, computed } from 'vue'
+
+// VARS
+const data = ref([
+  {
+    title: 'Нур-Султан',
+    value: 15,
+    children: []
+  },
+  {
+    title: 'Алматы',
+    value: 15,
+    children: [
+      {
+        title: 'Управление 1',
+        value: 15,
+        children: [
+          {
+            title: 'Отдел 1',
+            value: 15,
+            children: []
+          },
+          {
+            title: 'Отдел 2',
+            value: 15,
+            children: []
+          }
+        ]
+      },
+      {
+        title: 'Управление 2',
+        value: 15,
+        children: []
+      }
+    ]
+  },
+  {
+    title: 'Актау',
+    value: 5,
+    children: []
+  }
+])
+</script>
 
 <template>
-  <div>hellow world</div>
+  <div class="main">
+    <table class="table">
+      <thead class="table__header">
+        <th class=""></th>
+        <th>Общее количество</th>
+        <th>Фактическое количество</th>
+        <th>Действия</th>
+      </thead>
+      <tbody>
+        <TableRow :data="data" />
+      </tbody>
+    </table>
+  </div>
 </template>
+
+<style scoped>
+.main {
+  width: 70vw;
+  margin-inline: auto;
+}
+
+.table {
+  width: 100%;
+}
+
+.table th {
+  padding: 10px 20px;
+}
+
+.table__header {
+  background-color: #2d3e50;
+  color: white;
+}
+
+.table__header th {
+  text-align: start;
+}
+
+.table__header th:nth-child(1) {
+  width: 40%;
+}
+</style>
